1041af667c73dc02ee262c42d96d58cae25efc7c
Springfield-Lin <lolwut80@tutamail.com>
2026-09-23 06:38:36 +0000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -46,15 +46,15 @@ function Resolve-MSBuildPath {
     return $msbuildPath
 }
 
-#function Invoke-Step {
-    #param(
-        #[string]$Label,
-        #[scriptblock]$Action
+function Invoke-Step {
+    param(
+        [string]$Label,
+        [scriptblock]$Action
     )
 
-    #Write-Host "==> $Label" -ForegroundColor Cyan
-    #& $Action
-    #if ($LASTEXITCODE -ne 0) {
+    Write-Host "==> $Label" -ForegroundColor Cyan
+    & $Action
+    if ($LASTEXITCODE -ne 0) {
         #throw "Step failed: $Label"
     }
 }
