b65be32e63bc1e9fd515724ccb87c24d518ae3ec
Springfield-Lin <lolwut80@tutamail.com>
2026-09-23 06:49:51 +0000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -69,22 +69,6 @@ if ($EnableUpdateNotifications) {
     $buildArgs += '/p:EnableUpdateNotifications=true'
 }
 
-#Invoke-Step 'Install web-panel dependencies' {
-    #& $pnpm --dir $webPanelDir install --frozen-lockfile
-}
-
-#Invoke-Step 'Lint web-panel' {
-    #& $pnpm --dir $webPanelDir run lint
-}
-
-#Invoke-Step 'Build web-panel' {
-    #& $pnpm --dir $webPanelDir run build
-}
-
-#Invoke-Step 'Test web-panel' {
-    #& $pnpm --dir $webPanelDir exec vitest run
-}
-
 Invoke-Step 'Restore NuGet packages' {
     & $msbuild $solutionPath /m /t:Restore /p:RestorePackagesConfig=true
 }
